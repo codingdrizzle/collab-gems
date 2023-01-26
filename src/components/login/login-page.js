f2cce8e4d2640a8aa59aa4edddf8ecf60a89c968
Kurtis1955 <kurtisbill2000@gmail.com>
2023-01-26 12:25:30 +0000

    styling

--- src/components/login/login-page.js
+++ src/components/login/login-page.js
@@ -4,33 +4,30 @@ import {BiUser} from  "react-icons/bi";
 import {RiLock2Line} from "react-icons/ri";
 import {SlArrowRightCircle} from "react-icons/sl";
 import "./styles.css"
-import { Form,Input, message } from "antd";
-import { useState } from "react";
+import { Form, Input } from "antd";
 
  
 const LoginPage = ()=>{
-
-    // const Loginsucces=[message,setMessage]=useState ('');{
-    //     message= 'Login Successful'
-    // }
     
     return(
         <div className="login-container">
 
-            <Form>
+         
             <div className="first-group">
                     <img src={gems} alt=""/>
                     <h2>Login</h2>
             </div>
+
+            <Form>
             <div className="second-group">
                 <div className="card1">
                       <BiUser size={40}/>
-                      <Input required type="text" placeholder='Username'/>
-                      <a ><SlArrowRightCircle size={60}/></a>
+                      <Input id="username" required type="text" placeholder='Username'/> <a href=""><SlArrowRightCircle size={60}/></a>
+                      
                 </div>
                 <div className="card2">
                       <RiLock2Line size={40}/>
-                      <Input required type="password" placeholder='Password'/>
+                      <Input id="password" required type="password" placeholder='Password'/>
                 </div>
             </div>
             <div className="third-group">
