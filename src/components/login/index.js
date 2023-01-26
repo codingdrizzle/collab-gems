197da018a40d3e63926cee2f4b3d89c50e7fd55f
Kurtis1955 <kurtisbill2000@gmail.com>
2023-01-26 10:26:43 +0000

    login vibes

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,14 +2,8 @@ import React from "react";
 import LoginPage from "./login-page";
 
 const Login = ()=>{
-    return( 
-        <div>
-                    <LoginPage/>
-                    <div className="third-group">
-                 <a href="">Forget Password?</a>
-                 <h3>Don&apos;t have an account?<a href=""> Register</a></h3>
-            </div>
-        </div>
+    return(
+        <LoginPage/>
         
     )
 };
