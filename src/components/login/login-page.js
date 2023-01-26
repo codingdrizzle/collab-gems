430348e2511282a75d054924e7db1e4ac497476b
Kurtis1955 <kurtisbill2000@gmail.com>
2023-01-26 18:25:09 +0000

    done styling

--- src/components/login/login-page.js
+++ src/components/login/login-page.js
@@ -2,7 +2,7 @@ import React from "react";
 import gems from "../../asserts/images/gems.png";
 import {BiUser} from  "react-icons/bi";
 import {RiLock2Line} from "react-icons/ri";
-import {SlArrowRightCircle} from "react-icons/sl";
+import {BiLogIn} from "react-icons/bi";
 import "./styles.css"
 import { Form, Input } from "antd";
 
@@ -28,7 +28,7 @@ const LoginPage = ()=>{
                       <BiUser className="biuser" color="darkgrey" size={40}/>
                       <Input id="username" required type="text" placeholder='Username'/>                     
                 </div>
-                    <a className="login-link" href="" onClick={message}><SlArrowRightCircle size={60}/></a>
+                    <a className="login-link" href="" onClick={message}><BiLogIn size={60}/></a>
                 <div className="card2">
                       <RiLock2Line className="lock" color="darkgrey" size={40}/>
                       <Input id="password" required type="password" placeholder='Password'/>
