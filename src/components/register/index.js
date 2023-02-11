0b0b3cec95ad5c628f89e5e69f7c7d403cf5546d
Kurtis1955 <kurtisbill2000@gmail.com>
2023-02-11 01:16:13 +0000

    registration form on going

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Typography } from "antd";
+import img from "../../asserts/images/gems.png";
 
-const Register = ()=>{
+
+const Registration = ()=>{
+    const {Title} = Typography;
     return(
-        <div>
+        <div className="register-card">
+            <div className="register-header">
+                  <img src={img} alt=""/>
+                  <Title className="login-text" level={3}>Login</Title>
+                  <Title className="register-text" level={1}>Register</Title>
+            </div>
+            <div className="register-form">
 
+            </div>
         </div>
     )
 };
 
-export default Register;
+export default Registration;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
+import Registration from '../components/register';
 import Homepage from '../pages/home-page';
 import LoginPage from '../pages/login-page';
 
@@ -8,7 +9,7 @@ const AppRoutes = () => {
 		<Routes>
 			<Route path='/' element={<Homepage/>}/>
 			<Route path='/login' element={<LoginPage/>}/>
-			
+			<Route path='/register' element={<Registration/>}/>
 		</Routes>
 	);
 };
