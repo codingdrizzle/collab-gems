57677b6ebf915b4059973d23b20fbc3d8b8448d7
Kurtis1955 <kurtisbill2000@gmail.com>
2023-02-12 02:04:55 +0000

    register

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography,Col,Input } from 'antd';                                                                                                              
 import {CgUserList} from 'react-icons/cg';
 import {TfiEmail} from 'react-icons/tfi';
-import {CiUser,CiLock} from 'react-icons/ci';
+import {CiUser,CiLock } from 'react-icons/ci';
 import img from '../../asserts/images/gems.png';
 import './styles.css';
 
@@ -17,22 +17,22 @@ const Registration = ()=>{
             </div>
             <div className='register-form'>            
                   <Col span={24}>
-                    <Input type='text' placeholder='First name' size='large' required/> 
+                    <Input type='text' placeholder='First name' size='large' prefix={<CgUserList/>} required/> 
                   </Col>
                   <Col span={24}>
-                    <Input type='text' placeholder='Last name' size='large' required/> 
+                    <Input type='text' placeholder='Last name' size='large' prefix={<CgUserList/>} required/> 
                   </Col>
                   <Col span={24}>
-                    <Input type='text' placeholder='Email address' size='large' required/> 
+                    <Input type='text' placeholder='Email address' size='large' prefix={<TfiEmail/>} required/> 
                   </Col>
                   <Col span={24}>
-                    <Input type='text' placeholder='Username' size='large' required/>
+                    <Input type='text' placeholder='Username' size='large' prefix={<CiUser/>} required/>
                   </Col>      
                   <Col span={24}>
-                    <Input type='password' placeholder='Password' size='large' required/> 
+                    <Input type='password' placeholder='Password' size='large' prefix={<CiLock/>} required/> 
                   </Col> 
                   <Col span={24}>
-                    <Input type='password' placeholder='Re-enter Password' size='large' required/> 
+                    <Input type='password' placeholder='Re-enter Password' size='large' prefix={<CiLock/>} required/> 
                   </Col>                  
             </div>
         </div>
